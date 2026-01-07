57ed4f0fa573a4931a7381ea3c93a56595abac1d
yahia-y4 <yalhmwd994@gmail.com>
2026-01-07 19:49:48 +0300

    c5

--- src/app/storage/storageRightPart.tsx
+++ src/app/storage/storageRightPart.tsx
@@ -1,39 +1,171 @@
+"use client";
+import MyButton from "@/components/mybutton/myButton";
+import MyInput from "@/components/myInput/myInput";
+import MySelect from "@/components/mySelect/mySelect";
+import React, { useState } from "react";
 
-"use client"
-import MyButton from "@/components/mybutton/myButton"
-import MyInput from "@/components/myInput/myInput"
-import MySelect from "@/components/mySelect/mySelect"
-import { useState } from "react"
+type FormItemData = {
+  name?: string;
+  company?: string;
+  form?: string;
+  concent?: string;
+  concent_unit?: string;
+  titer?: string;
+  titer_unit?: string;
+  package?: string;
+  quantity?: number;
+  price_buy?: number;
+  profit?: number;
+  barcode?: string;
+  expire_date?: string;
+};
+export default function StorageRightPart() {
+  const [formItemData, setFormItemData] = useState<FormItemData>({
+    name: "",
+    company: "",
+    form: "",
+    concent: "",
+    concent_unit: "الواحدة",
+    titer: "",
+    titer_unit: "",
+    package: "",
+    quantity: 1,
+    price_buy: 0,
+    profit: 0,
+    barcode: "",
+    expire_date: "",
 
-export default function StorageRightPart() {  
-    const [formItemData, setFormItemData] = useState({})
-  return(
+  
+  });
+  function handleName(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, name: value });
+
+  }
+  function handleCompany(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, company: value });
+  }
+  function handleForm(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, form: value });
+
+  }
+  function handleConcent(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, concent: value });
+  }
+  function handleConcentUnit(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, concent_unit: value });
+
+  }
+  function handleTiter(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, titer: value });
+  }
+    function handleTiterUnit(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, titer_unit: value });
+    }
+  function handlePackage(e: React.ChangeEvent<HTMLSelectElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, package: value });}  
+
+    function handleQuantity(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = parseInt(e.target.value);
+    setFormItemData({ ...formItemData, quantity: value });
+  }
+  function handlePriceBuy(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = parseFloat(e.target.value);
+    setFormItemData({ ...formItemData, price_buy: value });
+  }
+    function handleProfit(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = parseFloat(e.target.value);
+    setFormItemData({ ...formItemData, profit: value });
+  }
+    function handleBarcode(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, barcode: value });
+  }
+    function handleExpireDate(e:React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setFormItemData({ ...formItemData, expire_date: value });
+    }
+
+function addclick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        e.preventDefault();
+      //add to storage logic
+      console.log(formItemData)
+    } 
+  return (
     <div className="storageRightPart">
-     <div className="topButsRightStorage"></div>
-     <form action="" className="formStorage">
-     <MyInput label_v={"الاسم التجاري"}></MyInput>
-     <MyInput label_v={"اسم الشركة"}></MyInput>
-     <p>الشكل الصيدلاني</p>
-     <MySelect options_v={["1", "2", "3"]}></MySelect>
-        <MyInput label_v={"التركيز"}>
-            <MySelect options_v={["mg", "ml", "g"]}></MySelect>
+      <div className="topButsRightStorage"></div>
+      <form action="" className="formStorage">
+        <MyInput
+            onChange={handleName}
+          input_v={formItemData.name}
+          label_v={"الاسم التجاري"}
+        ></MyInput>
+
+        <MyInput
+            onChange={handleCompany}
+          input_v={formItemData.company}
+          label_v={"اسم الشركة"}
+        ></MyInput>
+        <p style={{ fontSize: "18px" }}>الشكل الصيدلاني</p>
+        <MySelect onChange={handleForm} options_v={["أقراص","كبسول","شراب"]}></MySelect>
+
+
+        <MyInput onChange={handleConcent} input_v={formItemData.concent} label_v={"التركيز"}>
+          <MySelect value_v={formItemData.concent_unit} onChange={handleConcentUnit} options_v={["mg", "ml", "g"]}></MySelect>
         </MyInput>
-        <MyInput label_v={"العيار"}>
-            <MySelect options_v={["mg", "ml", "g"]}></MySelect>
+
+
+        <MyInput 
+        onChange={handleTiter}
+        input_v={formItemData.titer} label_v={"العيار"}>
+
+          <MySelect value_v={formItemData.titer_unit} onChange={handleTiterUnit} options_v={["mg", "ml", "g"]}></MySelect>
         </MyInput>
-        <MyInput label_v={"العبوة"}>
-            <MySelect options_v={["علبة", "قنينة", "شريط"]}></MySelect>
-        </MyInput>
-        <MyInput label_v={"الكمية"} type_v={"number"}></MyInput>
-        <MyInput label_v={"سعر الشراء"} type_v={"number"}></MyInput>
-        <MyInput label_v={"نسبة الربح %"} type_v={"number"}></MyInput>
-        <MyInput label_v={"الباركود"}></MyInput>
-        <MyInput label_v={"تاريخ الانتهاء"} type_v={"date"}></MyInput>
+        <p style={{ fontSize: "18px" }}>نوع العبوة</p>
+
+        <MySelect value_v={formItemData.package} onChange={handlePackage} options_v={["علبة", "قنينة", "شريط"]}></MySelect>
+
+        <MyInput
+          input_v={formItemData.quantity}
+        onChange={handleQuantity}
+          label_v={"الكمية"}
+          type_v={"number"}
+        ></MyInput>
+        <MyInput
+        onChange={handlePriceBuy}
+          input_v={formItemData.price_buy}
+          label_v={"سعر الشراء"}
+          type_v={"number"}
+        ></MyInput>
+        <MyInput
+        onChange={handleProfit}
+          input_v={formItemData.profit}
+          label_v={"نسبة الربح %"}
+          type_v={"number"}
+        ></MyInput>
+        <MyInput
+        onChange={handleBarcode}
+          input_v={formItemData.barcode}
+          label_v={"الباركود"}
+        ></MyInput>
+        <MyInput
+        onChange={handleExpireDate}
+          input_v={formItemData.expire_date}
+          label_v={"تاريخ الانتهاء"}
+          type_v={"date"}
+        ></MyInput>
         <div className="item-form-buts">
-            <MyButton>اضافة الى المخزن</MyButton>
-            <MyButton>مسح البيانات</MyButton>
+          <MyButton onClick={addclick}>اضافة الى المخزن</MyButton>
+          <MyButton>مسح البيانات</MyButton>
         </div>
-     </form>
+      </form>
     </div>
-  )
+  );
 }
--- src/components/mySelect/mySelect.tsx
+++ src/components/mySelect/mySelect.tsx
@@ -1,10 +1,10 @@
 import "./mySelect.css";
-export default function MySelect({ options_v=[] }) {
+export default function MySelect({ options_v=[],onChange,value_v}) {
    const options = options_v.map((option, index) => (
-    <option key={index}> {option}</option>
+    <option value={option} key={index}> {option}</option>
      
 
   ));
-  return (<select className="mySelect">
+  return (<select className="mySelect" onChange={onChange} value={value_v}>
     {options}
   </select>)}
