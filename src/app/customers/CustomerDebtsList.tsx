61eeeceb8692004415c9b9279e9c3490d2fc38e9
yahia-y4 <yalhmwd994@gmail.com>
2026-01-28 13:42:06 +0300

    c30

--- src/app/customers/CustomerDebtsList.tsx
+++ src/app/customers/CustomerDebtsList.tsx
@@ -3,32 +3,39 @@
 
 import "./customers.css"
 import MyTable from "@/components/myTable/myTable";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import {getDebtsCustomerApi} from "@/APIs/getDebtsCustomerApi";
+import { useContext } from "react"
+import { CustomersContext } from "@/app/customers/CustomersContext"
+import { formatDateTime } from "@/APIs/formatDateTime";
 export default function CustomerDebtsList() {
-    const [customerDebts,setCustomerDebts] = useState([
-        {
-            id:1,
-            amount:1000,
-            date:"2023-01-01",
-            note:"معجون اسنان",
-        },
-        {
-            id:2,
-            amount:2000,
-            date:"2023-02-01",
-            note:"معجون طبي",
-        },
-    ]);
+    const {selectedCustomer} = useContext(CustomersContext);
+    const [customerDebts,setCustomerDebts] = useState([]);
+
+    useEffect(()=>{
+        async function fetchCustomerDebts(){
+            const result = await getDebtsCustomerApi(selectedCustomer.id);
+            if(result.success){
+                const formatData = result.debts.map((debt: object) => ({
+                    ...debt,
+                    debt_date: formatDateTime(debt.debt_date),
+                }));
+                setCustomerDebts(formatData);
+            }
+
+        }
+        fetchCustomerDebts();
+    })
     /* ===== الأعمدة ===== */
     const columns=[
         { key: "id", title: "id" },
         { key: "amount", title: "المبلغ" },
-        { key: "date", title: "التاريخ" },
+        { key: "debt_date", title: "التاريخ" },
         {key:"note",title:"ملاحظة"},
     ];
     return (
         <div className="customer-debts-list">
-            <h2>قائمة ديون : {"فلان"}</h2>
+            <h2>قائمة ديون : {selectedCustomer?.name}</h2>
             <div className="customer-debts-list-table">
                 <MyTable columns={columns} data={customerDebts} />
             </div>
--- src/app/customers/CustomerPaymentsReceivedList.tsx
+++ src/app/customers/CustomerPaymentsReceivedList.tsx
@@ -2,32 +2,44 @@
 import "./customers.css"
 
 import MyTable from "@/components/myTable/myTable";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import {getReceivePaymentCustomerApi} from "@/APIs/getReceivePaymentCustomerApi";
+import { useContext } from "react"
+import { CustomersContext } from "@/app/customers/CustomersContext"
+import { formatDateTime } from "@/APIs/formatDateTime";
 export default function CustomerPaymentsReceivedList() {
-    const [customerPaymentsReceived,setCustomerPaymentsReceived] = useState([
-        {
-            id:1,
-            amount:1000,
-            date:"2023-01-01",
-            note:"دفعة اولى",
-        },
-        {
-            id:2,
-            amount:2000,
-            date:"2023-02-01",
-            note:"",
-        },
-    ]);
+    const {selectedCustomer} = useContext(CustomersContext);
+    const [customerPaymentsReceived,setCustomerPaymentsReceived] = useState([]);
+
+
+
+
+
+useEffect(()=>{
+        async function fetchCustomerPaymentsReceived(){
+            const result = await getReceivePaymentCustomerApi(selectedCustomer.id);
+            if(result.success){
+                const formatData = result.payments.map((payment: object) => ({
+                    ...payment,
+                    payment_date: formatDateTime(payment.payment_date),
+                }));
+                setCustomerPaymentsReceived(formatData);
+            }
+        }
+        fetchCustomerPaymentsReceived();
+    })
+
+
     /* ===== الأعمدة ===== */
     const columns=[
         { key: "id", title: "id" },
         { key: "amount", title: "المبلغ" },
-        { key: "date", title: "التاريخ" },
+        { key: "payment_date", title: "التاريخ" },
         {key:"note",title:"ملاحظة"},
     ];
     return (
         <div className="customer-debts-list">
-            <h2>قائمة مدفوعات : {"فلان"}</h2>
+            <h2>قائمة مدفوعات : {selectedCustomer?.name}</h2>
             <div className="customer-debts-list-table">
                 <MyTable columns={columns} data={customerPaymentsReceived} />
             </div>
